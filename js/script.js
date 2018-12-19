233e5ab0172ec702bf844d17be8a20e378ff382c
StefanGerrits2 <45566396+StefanGerrits2@users.noreply.github.com>
2018-12-19 20:48:42 +0100

    Fixes plus styling states form

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 /*eslint-env browser*/
 
 /*eslint 'no-console': 0*/
+
+var slider = document.getElementById("slider");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; 
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}						  
+									  
+/*Bron: https://www.w3schools.com/howto/howto_js_rangeslider.asp*/					
+
+
